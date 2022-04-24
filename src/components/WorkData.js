dcf65a2a5def657e860adc664ab7aba17a33e2f0
gefgu <gefgu@hotmail.com>
2022-04-24 16:54:57 -0300

    Add render of work mocking data

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -2,7 +2,31 @@ import React, { Component } from "react";
 
 class WorkData extends Component {
   render() {
-    return <div>Work</div>;
+    const workList = this.props.data;
+
+    return (
+      <div>
+        <h2>Work Experience</h2>
+        {workList.map((workInfo) => {
+          const {
+            companyName,
+            positionTitle,
+            description,
+            startDate,
+            endDate,
+          } = workInfo;
+
+          return (
+            <div>
+              <h3>{positionTitle}</h3>
+              <h4>{companyName}</h4>
+              <span>{startDate} - {endDate}</span>
+              <p>{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
