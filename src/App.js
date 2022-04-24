cd38955424099f5e0e007b71321f5eb5d45872a4
gefgu <gefgu@hotmail.com>
2022-04-24 16:44:56 -0300

    Add mocking of data of cv

--- src/App.js
+++ src/App.js
@@ -4,6 +4,46 @@ import WorkData from "./components/WorkData";
 import EducationalData from "./components/EducationalData";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        general: {
+          name: "Gustavo Bruno",
+          email: "gefgu12@gmail.com",
+          phone: "+55 (99) 99999-9999",
+        },
+        educational: [
+          {
+            schoolName: "Harvard Business School",
+            titleOfStudy: "Business Making",
+            startDate: "2016",
+            endDate: "2020",
+          },
+        ],
+        work: [
+          {
+            companyName: "Facebook",
+            positionTitle: "Senior Web Developer",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            startDate: "2021",
+            endDate: "2022",
+          },
+          {
+            companyName: "Google",
+            positionTitle: "Web Developer",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            startDate: "2020",
+            endDate: "2021",
+          },
+        ],
+      },
+    };
+  }
+
   render() {
     return (
       <div>
