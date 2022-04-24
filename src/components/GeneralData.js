171af8e01fcbf8e9d7ef1a0c93b3b08772534114
gefgu <gefgu@hotmail.com>
2022-04-24 16:54:40 -0300

    Add render of general mocking data

--- src/components/GeneralData.js
+++ src/components/GeneralData.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class GeneralData extends Component {
   render() {
-    return <div>General</div>;
+    const { name, email, phone } = this.props.data;
+    return (
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
+        <p>{phone}</p>
+      </div>
+    );
   }
 }
 
