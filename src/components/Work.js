cd445959529e7b7a3a145c8d18c48ac6f7934556
gefgu <gefgu@hotmail.com>
2022-04-28 19:43:32 -0300

    Transform Work into functional component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,121 +1,118 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/work.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 
-class Work extends Component {
-  render() {
-    const workList = this.props.data;
-    const { handleListEdit, enableListEdit, deleteListElement, addWork } =
-      this.props;
+function Work(props) {
+  const workList = props.data;
+  const { handleListEdit, enableListEdit, deleteListElement, addWork } = props;
 
-    return (
-      <div className="work-section">
-        <h2>
-          Work Experience{" "}
-          <button onClick={addWork}>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </h2>
-        {workList.map((workInfo, index) => {
-          const {
-            companyName,
-            positionTitle,
-            description,
-            startDate,
-            endDate,
-            editing,
-          } = workInfo;
+  return (
+    <div className="work-section">
+      <h2>
+        Work Experience{" "}
+        <button onClick={addWork}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </h2>
+      {workList.map((workInfo, index) => {
+        const {
+          companyName,
+          positionTitle,
+          description,
+          startDate,
+          endDate,
+          editing,
+        } = workInfo;
 
-          if (!editing) {
-            return (
-              <div key={index}>
-                <h3 className="position-title">
-                  {positionTitle}{" "}
-                  <button onClick={() => enableListEdit("work", index, true)}>
-                    <FontAwesomeIcon icon={faEdit} />
-                  </button>
-                </h3>
-                <h4 className="company-name">{companyName}</h4>
-                <span className="date">
-                  {startDate} - {endDate}
-                </span>
-                <p className="job-description">{description}</p>
-              </div>
-            );
-          }
-
+        if (!editing) {
           return (
-            <form key={index}>
+            <div key={index}>
+              <h3 className="position-title">
+                {positionTitle}{" "}
+                <button onClick={() => enableListEdit("work", index, true)}>
+                  <FontAwesomeIcon icon={faEdit} />
+                </button>
+              </h3>
+              <h4 className="company-name">{companyName}</h4>
+              <span className="date">
+                {startDate} - {endDate}
+              </span>
+              <p className="job-description">{description}</p>
+            </div>
+          );
+        }
+
+        return (
+          <form key={index}>
+            <input
+              className="position-title"
+              placeholder="Position Title"
+              value={positionTitle}
+              data-index={index}
+              onChange={(e) => handleListEdit(e, "work")}
+              name="positionTitle"
+            ></input>
+            <div>
               <input
-                className="position-title"
-                placeholder="Position Title"
-                value={positionTitle}
+                className="company-name"
+                placeholder="Company Name"
+                value={companyName}
                 data-index={index}
                 onChange={(e) => handleListEdit(e, "work")}
-                name="positionTitle"
+                name="companyName"
               ></input>
-              <div>
+              <span className="date">
                 <input
-                  className="company-name"
-                  placeholder="Company Name"
-                  value={companyName}
+                  placeholder="Start Date"
+                  value={startDate}
                   data-index={index}
                   onChange={(e) => handleListEdit(e, "work")}
-                  name="companyName"
+                  name="startDate"
                 ></input>
-                <span className="date">
-                  <input
-                    placeholder="Start Date"
-                    value={startDate}
-                    data-index={index}
-                    onChange={(e) => handleListEdit(e, "work")}
-                    name="startDate"
-                  ></input>
-                  -
-                  <input
-                    placeholder="End Date"
-                    value={endDate}
-                    data-index={index}
-                    onChange={(e) => handleListEdit(e, "work")}
-                    name="endDate"
-                  ></input>
-                </span>
-              </div>
-              <textarea
-                className="job-description"
-                placeholder="Job description..."
-                value={description}
-                data-index={index}
-                onChange={(e) => handleListEdit(e, "work")}
-                name="description"
-                rows={4}
-              ></textarea>
-              <div className="form-buttons">
-                <button
-                  type="submit"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    enableListEdit("work", index, false);
-                  }}
-                >
-                  Submit
-                </button>
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    deleteListElement("work", index);
-                  }}
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </div>
-            </form>
-          );
-        })}
-      </div>
-    );
-  }
+                -
+                <input
+                  placeholder="End Date"
+                  value={endDate}
+                  data-index={index}
+                  onChange={(e) => handleListEdit(e, "work")}
+                  name="endDate"
+                ></input>
+              </span>
+            </div>
+            <textarea
+              className="job-description"
+              placeholder="Job description..."
+              value={description}
+              data-index={index}
+              onChange={(e) => handleListEdit(e, "work")}
+              name="description"
+              rows={4}
+            ></textarea>
+            <div className="form-buttons">
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  enableListEdit("work", index, false);
+                }}
+              >
+                Submit
+              </button>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  deleteListElement("work", index);
+                }}
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
+          </form>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Work;
