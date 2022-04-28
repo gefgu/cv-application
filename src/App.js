1fd3aecae03f7812156a5feeade3f88dab5a4ed6
gefgu <gefgu@hotmail.com>
2022-04-28 19:50:50 -0300

    Add app function component and render sample data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import General from "./components/General";
 import Work from "./components/Work";
 import Education from "./components/Education";
@@ -149,4 +149,53 @@ class App extends Component {
   }
 }
 
+function HookApp() {
+  const [general, setGeneral] = useState({
+    name: "John Doe",
+    email: "John@gmail.com",
+    phone: "+55 (99) 99999-9999",
+    editing: false,
+  });
+
+  const [education, setEducation] = useState([
+    {
+      schoolName: "Harvard Business School",
+      titleOfStudy: "Business Making",
+      startDate: "2016",
+      endDate: "2020",
+      editing: false,
+    },
+  ]);
+
+  const [work, setWork] = useState([
+    {
+      companyName: "Facebook",
+      positionTitle: "Senior Web Developer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      startDate: "2021",
+      endDate: "2022",
+      editing: false,
+    },
+    {
+      companyName: "Google",
+      positionTitle: "Web Developer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      startDate: "2020",
+      endDate: "2021",
+      editing: false,
+    },
+  ]);
+
+  return (
+    <div className="cv-container">
+      <General data={general} />
+      <Work data={work} />
+      <Education data={education} />
+    </div>
+  );
+}
+
+export { HookApp };
 export default App;
