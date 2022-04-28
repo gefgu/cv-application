a5ffcb6812f9687361867c5cc692a64c41bc05ec
gefgu <gefgu@hotmail.com>
2022-04-28 19:44:34 -0300

    Transform General into functional component

--- src/components/General.js
+++ src/components/General.js
@@ -1,70 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/general.css";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class General extends Component {
-  render() {
-    const { name, email, phone, editing } = this.props.data;
-    const { handleEdit, enableEdit } = this.props;
+function General(props) {
+  const { name, email, phone, editing } = props.data;
+  const { handleEdit, enableEdit } = props;
 
-    if (!editing) {
-      return (
-        <div className="general-data">
-          <h1>{name}</h1>
-          <div className="contact-info">
-            <p>
-              <FontAwesomeIcon icon={faEnvelope} />
-              {email}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faPhone} /> {phone}
-            </p>
-          </div>
-          <button
-            onClick={() => {
-              enableEdit(true);
-            }}
-          >
-            Edit
-          </button>
+  if (!editing) {
+    return (
+      <div className="general-data">
+        <h1>{name}</h1>
+        <div className="contact-info">
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} />
+            {email}
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faPhone} /> {phone}
+          </p>
         </div>
-      );
-    } else if (editing) {
-      return (
-        <form className="general-data">
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={handleEdit}
-          ></input>
-          <div className="contact-info">
-            <div>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <input
-                type="text"
-                id="email"
-                value={email}
-                onChange={handleEdit}
-              ></input>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faPhone} />
-              <input
-                type="text"
-                id="phone"
-                value={phone}
-                onChange={handleEdit}
-              ></input>
-            </div>
+        <button
+          onClick={() => {
+            enableEdit(true);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    );
+  } else if (editing) {
+    return (
+      <form className="general-data">
+        <input type="text" id="name" value={name} onChange={handleEdit}></input>
+        <div className="contact-info">
+          <div>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={handleEdit}
+            ></input>
           </div>
-          <button className="submit" onClick={() => enableEdit(false)}>
-            Submit
-          </button>
-        </form>
-      );
-    }
+          <div>
+            <FontAwesomeIcon icon={faPhone} />
+            <input
+              type="text"
+              id="phone"
+              value={phone}
+              onChange={handleEdit}
+            ></input>
+          </div>
+        </div>
+        <button className="submit" onClick={() => enableEdit(false)}>
+          Submit
+        </button>
+      </form>
+    );
   }
 }
 
